9c2e29796a7ba181eb362fd2e6d68d4d715a4ecb
shugao0624 <gaoshuyang0624@gmail.com>
2021-09-16 12:41:18 -0400

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Shuyang Gao on 9/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
